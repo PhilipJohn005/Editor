465c0efe6a3385a4b1bab9cc9d50aca5590a51f4
PhilipJohn005 <pjdeluge123@gmail.com>
2026-02-18 01:53:00 +0530

    Feat: add shadcn dialog for transparency

--- src/components/screens/LandingPage/LandingPage.tsx
+++ src/components/screens/LandingPage/LandingPage.tsx
@@ -9,8 +9,28 @@ import Cta from './subComponents/Cta'
 import Footer04Page from './subComponents/Footer'
 import Navbar04Page from './subComponents/Navbar'
 import IntegrationsSection from './subComponents/Integrations'
+import { useEffect, useState } from "react";
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogFooter,
+} from "@/components/ui/dialog";
+import { Button } from "@/components/ui/button";
+
 
 const LandingPage = () => {
+  const [open, setOpen] = useState(false);
+
+  useEffect(() => {
+    const alreadyShown = localStorage.getItem("projectNoticeShown");
+    if (!alreadyShown) {
+      setOpen(true);
+      localStorage.setItem("projectNoticeShown", "true");
+    }
+  }, []);
+
   return (
     <div className='relative'>
         <div className="absolute left-24 top-0 bottom-70 w-px bg-gray-300" />
@@ -25,6 +45,27 @@ const LandingPage = () => {
         <FAQ05/>
         <Cta/>
         <Footer04Page/>
+        <Dialog open={open} onOpenChange={setOpen}>
+          <DialogContent className="rounded-2xl p-6">
+            <DialogHeader>
+              <DialogTitle className="text-xl font-semibold">
+                Project Status
+              </DialogTitle>
+            </DialogHeader>
+
+            <p className="text-gray-700 mt-2 leading-relaxed">
+              This project is actively under development. Parts like 
+              the canvas editor works
+            </p>
+
+            <DialogFooter className="mt-6">
+              <Button onClick={() => setOpen(false)} className="w-full">
+                Continue
+              </Button>
+            </DialogFooter>
+          </DialogContent>
+        </Dialog>
+
     </div>
   )
 }
